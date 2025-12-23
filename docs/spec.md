d861f508c44c257f7e414bd4e882737761c49074
Lionel Karlen <lionel.karlen@protonmail.com>
2025-12-23 18:22:37 +0100

    chore!: remove archive subcommand
    BREAKING CHANGE: Removed references to archive subcommand

--- docs/spec.md
+++ docs/spec.md
@@ -15,13 +15,3 @@ Usage:
 
 #### Arguments
 `--stdio`: used for helix and similar editors, that expect to take the file from standard input and output it to standard output.
-
-### Archive
-Archive all completed tasks from a file.
-
-Usage:
-`cria archive <from> <to>` -> write all completed tasks from the first file to the second.
-
-#### Arguments
-`--no-strip`: does not remove the `x` literal denoting a completed task
-`--date`: the date literal to be prepended to the task. Defaults to YYYY-MM-DD
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use clap::{Parser, Subcommand};
 use miette::Result;
 
-use crate::{
-    archive::ArchiveArgs,
-    format::{FormatArgs, run_format_command},
-};
-mod archive;
+use crate::format::{FormatArgs, run_format_command};
 mod format;
 mod todo;
 
@@ -21,8 +17,6 @@ enum Commands {
     /// Format the given file with `marked` entries first, regular entries second, completed entries last.
     /// `marked` entries are sorted in reverse order, the rest is left in file order.
     Format(FormatArgs),
-    /// Archive all completed tasks from a given file.
-    Archive(ArchiveArgs),
 }
 
 fn main() -> Result<()> {
@@ -30,6 +24,5 @@ fn main() -> Result<()> {
 
     match &cli.command {
         Commands::Format(format_args) => run_format_command(format_args),
-        Commands::Archive(archive_args) => todo!(),
     }
 }
